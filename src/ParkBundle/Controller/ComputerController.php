13e3919c41f6c39c300516b9051c6d03085214a9
formationProtoss <cristobal.protoss@gmail.com>
2015-10-06 15:25:35 +0200

    UV2 - Connexion BDD - Création commande

--- src/ParkBundle/Controller/ComputerController.php
+++ src/ParkBundle/Controller/ComputerController.php
@@ -7,6 +7,7 @@
  */
 namespace ParkBundle\Controller;
 
+use ParkBundle\Entity\Computer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -19,10 +20,11 @@ class ComputerController extends Controller
      */
     public function listAction()
     {
-        //List of computers
-        return array("computers" => $this->getComputerList());
+        $en = $this->getDoctrine()->getManager();
         //No data
         //return array("computers" => null);
+        //List of computers
+        return array("computers" => $this->getDoctrine()->getRepository('ParkBundle:Computer')->findAll());
     }
 
 
@@ -30,35 +32,67 @@ class ComputerController extends Controller
     {
         return array(
             0 => array(
-                    "id" => "1",
-                    "name" => "test",
-                    "ip" => "0.0.0.0",
-                    "enabled" => true
+                "name" => "Ordinateur 1",
+                "ip" => "192.168.0.1",
+                "enabled" => true
             ),
             1 => array(
-                    "id" => "2",
-                    "name" => "toto",
-                    "ip" => "1.1.1.1",
-                    "enabled" => false
+                "name" => "Ordinateur 2",
+                "ip" => "192.168.0.2",
+                "enabled" => false
             ),
             2 => array(
-                'id' => 3,
                 'name' => 'Ordinateur 3',
                 'ip' => '192.168.0.3',
                 'enabled' => true
             ),
                     3 => array(
-                'id' => 4,
                 'name' => 'Ordinateur 4',
                 'ip' => '192.168.0.4',
                 'enabled' => false
             ),
                     4 => array(
-                'id' => 5,
                 'name' => 'Ordinateur 5',
                 'ip' => '192.168.0.5',
                 'enabled' => true
             )
         );
     }
+
+    /**
+     * @Route("/park_test_add", name="park_test_add")
+     * @Template()
+     */
+    public function testAddAction()
+    {
+        $en = $this->getDoctrine()->getManager();
+        foreach($this->getComputerList() as $computer){
+            $testComp = new Computer();
+            $testComp->setName($computer['name']);
+            $testComp->setIp($computer['ip']);
+            $testComp->setEnabled($computer['enabled']);
+            $en->persist($testComp);
+        }
+        $en->flush();
+
+        die;
+    }
+
+    /**
+     * @Route("/park_test_remove", name="park_test_remove")
+     * @Template()
+     */
+    public function testRemoveAction()
+    {
+        $en = $this->getDoctrine()->getManager();
+        foreach($this->getDoctrine()->getRepository('ParkBundle:Computer')->findAll() as $computer){
+            //suppression
+            $en->remove($computer);
+        }
+        //commit
+        $en->flush();
+
+        die;
+    }
+
 }
